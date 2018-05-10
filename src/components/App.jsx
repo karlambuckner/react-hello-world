c4d73f72824bb9b91937dc93f2e3c499fd301bcd
Karla Buckner <kmb2662@gmail.com>
2018-05-10 11:43:44 -0700

    make first component for the header

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+
+function App(){
+  return (
+    <div>
+      <Header/>
+      <h3>3a</h3>
+      <h3>Karla</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+export default App;
