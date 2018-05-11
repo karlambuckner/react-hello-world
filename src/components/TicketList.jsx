6aee9c7fdecd104d1930f030e00d44843e140d04
Karla Buckner <kmb2662@gmail.com>
2018-05-11 11:13:39 -0700

    add props to the component

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -3,7 +3,10 @@ import Ticket from "./Ticket";
 
 function TicketList(){
   return(
-    <Ticket />
+    <Ticket
+      location="3A"
+      names="Karla"
+      issue="Firebase will not save record!"/>
   );
 }
 
